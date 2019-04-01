e81332be5779cb6a93ea0a79b67407c1db059255
Ivan <artemevskih91@mail.ru>
2019-04-01 17:16:06 +0500

    Это наш первый коммит

---
+++ lesson 1/1.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*var leftBoarderWidth = 1;
+{
+    let second = 2;
+}*/
+
+//const pi = 3.14;
+
+//console.log(pi);
+
+//let a = ['1.png','2.jpg','3.bmp'];
+
+//console.log(a[0]);
+
+//alert('Hello World!');
+
+//var answer = +prompt("Как дела?", "Хорошо");
+
+//console.log(typeof(answer));
+
+//let ans = confirm("Are you here?");
+
+//console.log(ans);
+
+//console.log(3 + "answer");
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
