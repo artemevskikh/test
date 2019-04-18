d77f8e84d56ea3fdc8e6de03fe93a6994963fc51
Ivan <artemevskih91@mail.ru>
2019-04-18 14:04:55 +0500

    Итоговое задание по первой части обучения

--- project main total/proj.js
+++ project main total/proj.js
@@ -1,17 +1,47 @@
 'use strict';
 
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'[0]),
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+    // mandatoryExp1 = document.querySelector('#expenses_1'),
+    // mandatoryExp2 = document.querySelector('#expenses_2'),
+    // mandatoryExp3 = document.querySelector('#expenses_3'),
+    // mandatoryExp4 = document.querySelector('#expenses_4'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
 let money, time;
 
-function start() {
-    money = +prompt("Ваш бюджет на месяц?");
+
+startBtn.addEventListener('click', function() {
     time = prompt("Введите дату в формате YYYY-MM-DD");
+    money = +prompt("Ваш бюджет на месяц?");
 
     while(isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?");
     }
-};
 
-start();
+    appData.budget = money;
+    appData.timeData = time;
+    budgetValue.textContent = money.toFixed();
+});
 
 var appData = {
     budget: money,
